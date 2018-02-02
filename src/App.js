6f12a3dd50438a802567d60f5ca996a006206ece
Benknightdark <moon791017@gmail.com>
2018-02-02 16:31:58 +0800

    add functional programing

---
+++ src/App.js
@@ -0,0 +1,107 @@
+import React, { Component } from "react";
+import list from "./list";
+import logo from "./logo.svg";
+import "./App.css";
+function isSearched(searchTerm) {
+  return function(item) {
+    return (
+      !searchTerm || item.value.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  };
+}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: list,
+      searchTerm: ""
+    };
+    this.RemoveItem = this.RemoveItem.bind(this);
+    this.searchValue = this.searchValue.bind(this);
+  }
+  RemoveItem(id) {
+    const isNotId = item => item.ObjectID !== id;
+    const UpdatList = this.state.list.filter(isNotId);
+    this.setState({ list: UpdatList });
+  }
+  searchValue(event) {
+    this.setState({ searchTerm: event.target.value });
+    console.log(event);
+  }
+  render() {
+    const { list, searchTerm } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to ds1111fdsf</h1>
+        </header>
+        <div className="App-intro">
+          <Search onChange={this.searchValue} value={searchTerm}>
+            search me
+          </Search>
+          <Table
+            list={list}
+            searchTerm={searchTerm}
+            RemoveItem={this.RemoveItem}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+//Reuse button Component
+// class Button extends Component {
+//   render() {
+//     const{onClick,children}=this.props
+//     return (
+//       <button onClick={onClick}>
+//         {children}
+//       </button>
+//     );
+//   }
+// }
+
+//button reuse function 
+// function Button({onClick,children}){
+//   return (
+//     <button onClick={onClick}>
+//       {children}
+//     </button>
+//   );
+// }
+
+//es6 button reuse function 
+const Button = ({ onClick, children }) => <button onClick={onClick}>{children}</button>;
+
+class Search extends Component {
+  render() {
+    const { onChange, value, children } = this.props;
+    return (
+      <form>
+        {children}
+        <input type="text" onChange={onChange} value={value} />
+      </form>
+    );
+  }
+}
+class Table extends Component {
+  render() {
+    const { list, searchTerm, RemoveItem } = this.props;
+    return (
+      <div>
+        {list.filter(isSearched(searchTerm)).map(item => (
+          <div key={item.ObjectID}>
+            <Button type="button" onClick={() => RemoveItem(item.ObjectID)}>
+              aa
+            </Button>
+            <span>{item.text}</span>
+            <span>{item.value}</span>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/list.js
@@ -0,0 +1,18 @@
+const list = [
+  {
+    ObjectID: "1",
+    text: "abcddddd  ",
+    value: "fadfasdfasdfasdf"
+  },
+  {
+    ObjectID: "2",
+    text: "b  ",
+    value: "e"
+  },
+  {
+    ObjectID: "3",
+    text: "c  ",
+    value: "d"
+  }
+];
+export default list;
